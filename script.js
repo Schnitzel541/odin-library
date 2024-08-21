4342c9b3862e45d80d63e1cd05f2f4cb21a7f7f5
Schnitzel541 <j.budding122@gmail.com>
2024-08-21 20:07:23 +0200

    Add functionality to delete and toggle read | not read

--- script.js
+++ script.js
@@ -1,71 +1,112 @@
-const bookCardContainer = document.querySelector('.container');
+const bookCardContainer = document.querySelector(".container");
 
-const myLibrary = []
+let myLibrary = [];
 
-function Book(name, author, pages, read) {
-    this.name = name,
-    this.author = author,
-    this.pages = pages,
-    this.read = read;
+function Book(name, author, pages, read, uniqueId) {
+  (this.name = name),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read),
+    (this.uniqueId = Book.incrementId());
+}
+
+
+Book.currentId = 0;
+Book.incrementId = function () {
+  return Book.currentId++;
 };
 
 function displayBook(arr) {
-    const bookCardContainer = document.querySelector('.container');
-    bookCardContainer.innerHTML = "";
-    for (let i = 0; i < arr.length; i++) {
-        const books = document.createElement("div");
-        books.classList.add("bookCard")
-        bookCardContainer.appendChild(books);
+  const bookCardContainer = document.querySelector(".container");
+  bookCardContainer.innerHTML = "";
+  for (let i = 0; i < arr.length; i++) {
+    const books = document.createElement("div");
+    books.classList.add("bookCard");
+    bookCardContainer.appendChild(books);
 
-        const bookName = document.createElement("div");
-        bookName.classList.add("bookText");
-        bookName.innerHTML = `<strong>Name:</strong> ${arr[i].name}`
-        books.appendChild(bookName);
+    const bookName = document.createElement("div");
+    bookName.classList.add("bookText");
+    bookName.innerHTML = `<strong>Name:</strong> ${arr[i].name}`;
+    books.appendChild(bookName);
 
-        const bookAuthor = document.createElement("div");
-        bookAuthor.classList.add("bookText");
-        bookAuthor.innerHTML = `<strong>Name:</strong> ${arr[i].author}`
-        books.appendChild(bookAuthor);
+    const bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("bookText");
+    bookAuthor.innerHTML = `<strong>Name:</strong> ${arr[i].author}`;
+    books.appendChild(bookAuthor);
 
-        const bookPages = document.createElement("div");
-        bookPages.classList.add("bookText");
-        bookPages.innerHTML = `<strong>Pages:</strong> ${arr[i].pages}`
-        books.appendChild(bookPages);
+    const bookPages = document.createElement("div");
+    bookPages.classList.add("bookText");
+    bookPages.innerHTML = `<strong>Pages:</strong> ${arr[i].pages}`;
+    books.appendChild(bookPages);
 
-        const bookButtons = document.createElement("div");
-        bookButtons.classList.add("buttonContainer");
-        bookButtons.innerHTML = `<button>Delete</button><button>Read</button>`
-        books.appendChild(bookButtons)
+    const bookButtons = document.createElement("div");
+    bookButtons.classList.add("buttonContainer");
+    bookButtons.innerHTML = `<button id="deleteBookBtn" onclick="deleteBookBtn(${arr[i].uniqueId})">Delete</button><button id="readBtn" name="${arr[i].uniqueId}" onclick="flipRead(${arr[i].uniqueId})">${arr[i].read ? "Read" : "Not Read"}</button>`;
+    books.appendChild(bookButtons);
 
-        const buttons = document.querySelectorAll("button");
-        buttons.forEach((button) => {
-            button.classList.add("bookButtons")
-        })
-    }
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.classList.add("bookButtons");
+    });
+  }
 }
-
-
 function addBookToLibrary() {
-    const nameInput = document.querySelector("#bookNameInput").value;
-    const authorInput = document.querySelector("#bookAuthorInput").value;
-    const pagesInput = document.querySelector("#bookPagesInput").value;
+  const nameInput = document.querySelector("#bookNameInput").value;
+  const authorInput = document.querySelector("#bookAuthorInput").value;
+  const pagesInput = document.querySelector("#bookPagesInput").value;
 
-    const newBook = new Book(nameInput, authorInput, pagesInput, false);
-    myLibrary.push(newBook);
-    displayBook(myLibrary);
+  const newBook = new Book(nameInput, authorInput, pagesInput, false);
+  myLibrary.push(newBook);
+  displayBook(myLibrary);
+  console.log(newBook);
 }
 
-const modal = document.querySelector(".modal")
+const lotr = new Book("Lord of the Rings", "1137", "J.R.R. Tolkien", false)
+myLibrary.push(lotr);
+
+
+const modal = document.querySelector(".modal");
 const openModalButton = document.querySelector("#openModal");
-openModalButton.addEventListener('click', () => {
-    modal.showModal();
-})
+openModalButton.addEventListener("click", () => {
+  const bookNameInput = document.querySelector("#bookNameInput");
+  const bookAuthorInput = document.querySelector("#bookAuthorInput");
+  const bookPagesInput = document.querySelector("#bookPagesInput");
+  bookNameInput.value = "";
+  bookAuthorInput.value = "";
+  bookPagesInput.value = "";
+  
+  modal.showModal();
+});
 
 const addBookButton = document.querySelector("#addBookButton");
-addBookButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    addBookToLibrary();
-    modal.close();
-})
+addBookButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+  modal.close();
+});
+
+function getLastInArray() {
+  const lastIndex = myLibrary[myLibrary.length - 1];
+  return lastIndex;
+}
+
+function deleteBookBtn(uniqueId) {
+    const bookIndex = myLibrary.findIndex((book) => book.uniqueId === uniqueId);
+
+    if (!bookIndex !== -1) {
+      myLibrary.splice(bookIndex, 1)
+    }
+
+    displayBook(myLibrary);
+  }
+
+function flipRead(uniqueId) {
+  const bookIndex = myLibrary.findIndex((book) => book.uniqueId === uniqueId);
+
+  if (bookIndex !== -1) {
+    myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+  }
+  displayBook(myLibrary);
+}
 
 displayBook(myLibrary);
